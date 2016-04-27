828eed680a41b4925b8340ef147663211230cfaf
yoendry <yfuentesh7@gmail.com>
2016-04-27 19:13:27 -0400

    Create my profile page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,4 +3,8 @@
  */
 $(function(){
     $('ul.tabs').tabs();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 100 // Creates a dropdown of 15 years to control year
+    });
 });
