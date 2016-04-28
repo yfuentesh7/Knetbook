46b2c0d3a1b16017ec9ca995efc8dbbfff053b56
yoendry <yfuentesh7@gmail.com>
2016-04-28 13:52:33 -0400

    Comment of publications finish

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,6 @@
  * Created by yoendry on 27/04/16.
  */
 $(function(){
-    $('ul.tabs').tabs();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 100 // Creates a dropdown of 15 years to control year
